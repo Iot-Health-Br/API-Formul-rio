78cbb404fc687e67d9b8b895a8735ac4cde10bb7
Iot-Health-Br <igl_macedo@hotmail.com>
2024-09-08 14:09:21 -0300

    Código Inicial, API que integrará com o front end em angular

---
+++ src/main/java/com/SpringAPIFormulario/SpringApiFormularioApplication.java
@@ -0,0 +1,13 @@
+package com.SpringAPIFormulario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiFormularioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiFormularioApplication.class, args);
+	}
+
+}
